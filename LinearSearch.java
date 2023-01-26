8725edfc98efdc8cc07283807ad69533479a25af
Binay <81138092+Binay1510@users.noreply.github.com>
2023-01-26 14:36:34 +0530

    linear search

---
+++ LinearSearch.java
@@ -0,0 +1,27 @@
+package ls;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] nums ={23,55,12,9,-7,10,-8,14};
+        int target=55;
+        int ans=linears(nums,target);
+        System.out.println(ans);
+    }
+    // search array :return index if item found
+    // otherwise if item not found return -1
+    static int  linears(int[] arr, int target){
+        if(arr.length==0){
+            return -1;
+
+        }
+        for (int index = 0; index < arr.length; index++) {
+            //check for elemet at every index if it is = target
+             int element = arr[index];
+             if(element== target){
+                 return index;
+             }
+        }
+        // if element is not found in the arr then
+        return -1;
+    }
+}
